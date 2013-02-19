35efe1dd5c29895e99d470e8867c16c320a507d8
Anirban Nandy <anirban.nandy@technicise.com>
2013-02-19 19:08:32 +0530

    Added Twitter in DNBSN

--- code/DNBSNGUI/MainWindow.xaml.cs
+++ code/DNBSNGUI/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using TweetSharp;
+using System.Diagnostics;
+
 
 namespace DNBSNGUI
 {
@@ -154,6 +157,37 @@ namespace DNBSNGUI
             //loginExpndr.IsEnabled = true;
             //loginExpndr.Header = "Logged In";
             //loginExpndr.IsExpanded = true;
+        }
+
+        TwitterService service;
+        OAuthRequestToken requestToken;
+
+        private void launchTwitterAppButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Pass your credentials to the service
+            service = new TwitterService("rfeDMlcxMQSqDSdmXDT3A", "7ZUD4YtVqOfFJcCHwWz7q0qTFjcKyVGSNyN4ElCsZZE");
+
+            // Step 1 - Retrieve an OAuth Request Token
+            requestToken = service.GetRequestToken();
+
+            // Step 2 - Redirect to the OAuth Authorization URL
+            Uri uri = service.GetAuthorizationUri(requestToken);
+            Process.Start(uri.ToString());
+        }
+
+        private void verifyTwitterApp_Click(object sender, RoutedEventArgs e)
+        {
+            // Step 3 - Exchange the Request Token for an Access Token            
+            OAuthAccessToken access = service.GetAccessToken(requestToken, verificationCodeTxtBox.Text);
+
+            // Step 4 - User authenticates using the Access Token
+            service.AuthenticateWith(access.Token, access.TokenSecret);
+
+            var tweets = service.ListTweetsOnHomeTimeline(new ListTweetsOnHomeTimelineOptions());
+            foreach (var tweet in tweets)
+            {
+                tweetsTextBlock.Text += tweet.User.ScreenName + " says " + tweet.Text + "\n";
+            }
         }
     }
 }
